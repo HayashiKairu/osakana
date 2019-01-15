f655cc659dcaed7a2d5e229730fc707dcf181166
taka666 <nkcgta.takahiro.enjo@gmail.com>
2019-01-15 13:52:00 +0900

    a

---
+++ kouki/Assets/Title/titleMG.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class titleMG : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     
+
+    }
+    public void startButton()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Demo_Scene");
+        }
+    }
+    public void sousaButton()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Demo_Scene");
+        }
+    }
+    public void EndButton()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.Quit();
+        }
+    }
+}

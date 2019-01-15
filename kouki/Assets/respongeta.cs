2e6732ae5f735a8325f4488f4883f19eddfe8c0c
taka666 <nkcgta.takahiro.enjo@gmail.com>
2019-01-15 11:52:33 +0900

    初期ドアの開閉

---
+++ kouki/Assets/respongeta.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respongeta : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        		
+	}
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "pleyer")
+        {
+             transform.position +=new Vector3(0, 0.015f,0);
+            
+            
+        }
+
+    }
+}
